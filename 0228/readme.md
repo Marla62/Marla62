6e5145b129dc5c4bcbd79669d6a1c00d5e5fc449
chen xiang.jiang <chenxiang.jiang@eeoa.com>
2022-02-28 14:16:39 +0800

    feat:组件抽离

--- 0228/readme.md
+++ 0228/readme.md
@@ -2,3 +2,26 @@ Core React Concepts
 
 - Component Composition
 - React Dev Tools
+
+组件传参数的快速方法
+    - 传统方式
+    <Pet
+        name={pet.name}
+        animal={pet.animal}
+        breed={pet.breed}
+        id={pet.id}
+        key={pet.id}
+        images={pet.images}
+        location={`${pet.city},${pet.state}`}
+    />
+    - 展开运算符
+    <Pet {...pet} key={pet.id} />
+
+
+pnpm
+
+什么是pnpm?
+
+对比yarn，npm优势？
+
+自身缺陷：
---
+++ 0228/src/Results.js
@@ -0,0 +1,30 @@
+import Pet from "./Pet";
+
+function Results({pets}) {
+    return (
+        <div className="search">
+            { 
+                !pets.length ?
+                (<h2>Not Pet Found</h2>) :
+                (
+                    pets.map((pet) => (
+                        // <Pet
+                        //   name={pet.name}
+                        //   animal={pet.animal}
+                        //   breed={pet.breed}
+                        //   id={pet.id}
+                        //   key={pet.id}
+                        //   images={pet.images}
+                        //   location={`${pet.city},${pet.state}`}
+                        // />
+
+                        <Pet {...pet} key={pet.id} />
+                      ))
+                ) 
+            }
+        </div>
+           
+    )
+}
+
+export default Results;
